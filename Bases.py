b93d57e276139eb1d39636f9e2256a01356fdd32
chasi650z <chasi64.z@gmail.com>
2024-06-23 12:35:46 -0500

    Primeras prácticas

---
+++ Bases.py
@@ -0,0 +1,21 @@
+#D1
+# con comillas si es texto
+print("Hello Word")
+# sin comillas cuando son números
+print(1234)
+# para colocar comillas dentro de un texto se utilizan '' comillas simples 
+print("Good Afernoon 'Señor'")
+# generar el resultado / sin comillas se ejecuta la operación
+print (100+50)
+
+#Cadenas de texto \n salto de linea \t tabular si quiero imprimir una \ debo colocar 2 \\
+print("Hello "+"Pablo\n Estamos prácticando")
+print("\t Practice")
+
+
+#Imput
+
+print("Tu nombre es "+ input(" Enter your name: "))
+print("Tu Apellido es "+ input("Enter your lastname: "))
+
+print("Tu nombre es "+ input("Enter your name: ")+ " "+ input("Enter your lastname: "))
